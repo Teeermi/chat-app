b2af8d20fac3f48efa79dc6a09663c0ab692dbeb
Termi <olafkrawczyk13@icloud.com>
2025-02-20 21:31:38 +0100

    Sockets

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ import { Server } from "socket.io";
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
 const port = 3000;
-// when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
+
+const chats = [
+    {id: 123, messages: [{user: "Termi", message: "Hello"}, {user: "Jopek", message: "Siemano"}]},
+    {id: 321, messages: [{user: "Termi", message: "Hello"}, {user: "Jopek", message: "Siemano"}]}
+]
 
 app.prepare().then(() => {
   const httpServer = createServer(handler);
@@ -15,7 +19,39 @@ app.prepare().then(() => {
   const io = new Server(httpServer);
 
   io.on("connection", (socket) => {
-    // ...
+    
+
+    socket.on("joinRoom", (roomId) => {
+        socket.join(roomId);
+        console.log(`Socket ${socket.id} dołączył do pokoju ${roomId}`);
+
+        const chat = chats.find((chat) => chat.id == roomId);
+
+          if (!chat) {
+            chats.push({id: roomId, messages: []});
+          }
+
+        
+    io.to(roomId).emit("reciveMessages", chat);
+  });
+
+    socket.on("sendMessage", ({ roomId, message, user }) => {
+        const chat = chats.find((chat) => chat.id == roomId);
+
+      
+
+    
+
+        chat.messages.push({user, message});
+        io.to(roomId).emit("reciveMessages", chat);
+    });
+
+
+ 
+    socket.on("disconnect", () => {
+      console.log(`User disconnected: ${socket.id}`);
+      io.emit("userDisconnected", { id: socket.id });
+    });
   });
 
   httpServer
