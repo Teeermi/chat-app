0e9d1e176e84637372295e872cef91074a84db53
Termi <olafkrawczyk13@icloud.com>
2025-02-23 18:45:38 +0100

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,54 @@
 "use client";
-import {SignIn} from "@/components/sign-in";
+import { SignIn } from "@/components/sign-in";
 
-import { Input } from "@/components/ui/input"
-import {ModeToggle} from "@/components/mode";
-import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+import { ModeToggle } from "@/components/mode";
+import { Button } from "@/components/ui/button";
 
 import Form from "next/form";
-import {useEffect, useState} from "react";
-import {getSession, moveToRoom} from "@/lib/actions";
+import { useEffect, useState } from "react";
+import { getSession, moveToRoom } from "@/lib/actions";
 import { Session } from "next-auth";
 import UserProfile from "@/components/UserProfile";
 
-
+import Loader from "@/components/loading";
 
 export default function Home() {
-const [session, setSession] = useState<Session | null>(null);
+  const [session, setSession] = useState<Session | null>(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        async function fetchData() {
-            const response = await getSession();
-            setSession(response);
-        }
-        fetchData();
-    }, []);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getSession();
+      setSession(response);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
 
-
-
+  if (loading) return <Loader />;
 
   if (session) {
     return (
-        <div className="mainWrapper flex flex-col items-center mb-[-20rem] ">
-            <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-                Enter a room
-            </h2>
+      <div className="mainWrapper flex flex-col items-center mb-[-20rem] ">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          Enter a room
+        </h2>
 
-
-            <Form action={moveToRoom} className="flex gap-2 mb-[25rem]" >
-                <Input type="number" placeholder="Code" name="code"/>
-                <Button type="submit">Move</Button>
-            </Form>
+        <Form action={moveToRoom} className="flex gap-2 mb-[25rem]">
+          <Input type="number" max={999} placeholder="Code" name="code" />
+          <Button type="submit">Move</Button>
+        </Form>
 
         <UserProfile session={session} />
-        </div>
-    )
-
-
-
-
-  } else  {
+      </div>
+    );
+  } else {
     return (
-        <main className="flex gap-2" >
-          <SignIn />
-          <ModeToggle />
-        </main>
+      <main className="flex gap-2">
+        <SignIn />
+        <ModeToggle />
+      </main>
     );
   }
-
-
-
-
-
 }
