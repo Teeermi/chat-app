583a850042235177937f6c41021a68c7a1dddd87
Termi <olafkrawczyk13@icloud.com>
2025-02-23 18:47:11 +0100

    Websockets

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ import next from "next";
 import { Server } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = "192.168.0.52";
 const port = 3000;
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
-const chats = [
-    {id: 123, messages: [{user: "Termi", message: "Hello"}, {user: "Jopek", message: "Siemano"}]},
-    {id: 321, messages: [{user: "Termi", message: "Hello"}, {user: "Jopek", message: "Siemano"}]}
-]
+const chats = [];
 
 app.prepare().then(() => {
   const httpServer = createServer(handler);
@@ -19,35 +16,42 @@ app.prepare().then(() => {
   const io = new Server(httpServer);
 
   io.on("connection", (socket) => {
-    
-
     socket.on("joinRoom", (roomId) => {
-        socket.join(roomId);
-        console.log(`Socket ${socket.id} dołączył do pokoju ${roomId}`);
+      socket.join(roomId);
+      console.log(`Socket ${socket.id} dołączył do pokoju ${roomId}`);
 
-        const chat = chats.find((chat) => chat.id == roomId);
+      const chat = chats.find((chat) => chat.id == roomId);
 
-          if (!chat) {
-            chats.push({id: roomId, messages: []});
-          }
+      if (!chat) {
+        chats.push({ id: roomId, messages: [] });
+      }
 
-        
-    io.to(roomId).emit("reciveMessages", chat);
-  });
-
-    socket.on("sendMessage", ({ roomId, message, user }) => {
-        const chat = chats.find((chat) => chat.id == roomId);
-
-      
-
-    
-
-        chat.messages.push({user, message});
-        io.to(roomId).emit("reciveMessages", chat);
+      io.to(roomId).emit("reciveMessages", chat);
     });
 
+    socket.on(
+      "sendMessage",
+      ({
+        roomId,
+        message,
+        user,
+        image,
+        date,
+        fileInfo: { filePath, fileType, fileName },
+      }) => {
+        const chat = chats.find((chat) => chat.id == roomId);
 
- 
+        chat.messages.push({
+          user,
+          message,
+          image,
+          date,
+          fileInfo: { filePath, fileType, fileName },
+        });
+        io.to(roomId).emit("reciveMessages", chat);
+      }
+    );
+
     socket.on("disconnect", () => {
       console.log(`User disconnected: ${socket.id}`);
       io.emit("userDisconnected", { id: socket.id });
